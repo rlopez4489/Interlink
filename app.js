90e963d948da177e58c4067c103ed987e0b37b06
rube4489 <ruben.lopez@doc24.com.ar>
2022-07-21 19:45:07 -0300

    ambios

--- app.js
+++ app.js
@@ -1,64 +1,12 @@
-// The typewriter element
-var typeWriterElement = document.getElementById("typewriter");
+const flagsElements = document.getElementById("flags");
 
-// The TextArray:
-var textArray = [
-  "Somos asesores expertos en inversión",
-  "Buscamos los modelos de negocios más rentables de USA",
-  "Construimos tu plan de negocio y tu sociedad",
-];
+flagsElements.addEventListener("click", (e) => {
+  console.log(e.target.parentElement.dataset.language);
+});
 
-// You can also do this by transfering it through a data-attribute
-// var textArray = typeWriterElement.getAttribute('data-array');
-
-// function to generate the backspace effect
-function delWriter(text, i, cb) {
-  if (i >= 0) {
-    typeWriterElement.innerHTML = text.substring(0, i--);
-    // generate a random Number to emulate backspace hitting.
-    var rndBack = 10 + Math.random() * 50;
-    setTimeout(function () {
-      delWriter(text, i, cb);
-    }, rndBack);
-  } else if (typeof cb == "function") {
-    setTimeout(cb, 800);
-  }
-}
-
-// function to generate the keyhitting effect
-function typeWriter(text, i, cb) {
-  if (i < text.length + 1) {
-    typeWriterElement.innerHTML = text.substring(0, i++);
-    // generate a random Number to emulate Typing on the Keyboard.
-    var rndTyping = 250 - Math.random() * 300;
-    setTimeout(function () {
-      typeWriter(text, i++, cb);
-    }, rndTyping);
-  } else if (i === text.length + 1) {
-    setTimeout(function () {
-      delWriter(text, i, cb);
-    }, 1000);
-  }
-}
-
-// the main writer function
-function StartWriter(i) {
-  if (typeof textArray[i] == "undefined") {
-    setTimeout(function () {
-      StartWriter(0);
-    }, 1000);
-  } else if (i < textArray[i].length + 1) {
-    typeWriter(textArray[i], 0, function () {
-      StartWriter(i + 1);
-    });
-  }
-}
-// wait one second then start the typewriter
-setTimeout(function () {
-  StartWriter(0);
-}, 1000);
-
-// counter
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`./languages/${language}.json`);
+};
 
 function counter(id, start, end, duration) {
   let obj = document.getElementById(id),
