a57ed12db59b87d553e430f0c12b7989d6054d5a
rube4489 <ruben.lopez@doc24.com.ar>
2022-09-27 08:27:28 -0300

    Cambios

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const flagsElements = document.getElementById("flags");
 const textsToChange = document.querySelectorAll("[data-section]");
 
 const changeLanguage = async (language) => {
-  const requestJson = await fetch(`./languages/${language}.json`);
-  const texts = await requestJson.json();
-  for (const textToChange of textsToChange) {
-    const section = textToChange.dataset.section;
-    const value = textToChange.dataset.value;
-    textToChange.innerHTML = texts[section][value];
+  console.log(language);
+  if (language === "en") {
+    location.href = "en/index.html";
+  } else {
+    location.href = "../index.html";
   }
 };
 
