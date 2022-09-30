040d1f3dac1d9e8cf079143b8790ed055b25b9f1
rube4489 <ruben.lopez@doc24.com.ar>
2022-09-30 16:56:13 -0300

    Cambio

--- app.js
+++ app.js
@@ -113,4 +113,7 @@ const phoneInputField = document.getElementById("Phone");
 const phoneInput = window.intlTelInput(phoneInputField, {
   utilsScript:
     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+  autoHideDialCode: false,
+  nationalMode: false,
+  preferredCountries: ["us"],
 });
