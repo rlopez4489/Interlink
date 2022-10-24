583f95b5ccfdbe7040bd96f92acab653518788a4
rube4489 <ruben.lopez@doc24.com.ar>
2022-10-23 23:22:14 -0300

    Cambios

--- app.js
+++ app.js
@@ -5,74 +5,72 @@ const textsToChange = document.querySelectorAll("[data-section]");
 const changeLanguage = async (language) => {
   if (language === "en") {
     if (location.pathname === "/nosotros") {
-      return (location.href = "./en/us.html");
-    } else if (location.pathname === "/services.html") {
-      return (location.href = "./en/services.html");
-    } else if (location.pathname === "/franchise.html") {
-      return (location.href = "./en/franchise.html");
-    } else if (location.pathname === "/franchise-texas.html") {
-      return (location.href = "./en/franchise-texas.html");
-    } else if (location.pathname === "/franchise-florida.html") {
-      return (location.href = "./en/franchise-florida.html");
-    } else if (location.pathname === "/franchise-california.html") {
-      return (location.href = "./en/franchise-california.html");
-    } else if (location.pathname === "/investor-visa.html") {
-      return (location.href = "./en/investor-visa.html");
-    } else if (location.pathname === "/investor-visa.html") {
-      return (location.href = "./en/investor-visa.html");
-    } else if (location.pathname === "/visa-e-2.html") {
-      return (location.href = "./en/visa-e-2.html");
-    } else if (location.pathname === "/visa-e-1.html") {
-      return (location.href = "./en/visa-e-1.html");
-    } else if (location.pathname === "/visa-eb-5.html") {
-      return (location.href = "./en/visa-eb-5.html");
-    } else if (location.pathname === "/visa-l-1.html") {
-      return (location.href = "./en/visa-l-1.html");
-    } else if (location.pathname === "/blog.html") {
-      return (location.href = "./en/blog.html");
-    } else if (location.pathname === "/faqs.html") {
-      return (location.href = "./en/faqs.html");
-    } else if (location.pathname === "/courses.html") {
-      return (location.href = "./en/courses.html");
-    } else if (location.pathname === "/contact.html") {
-      return (location.href = "./en/contact.html");
+      return (location.href = "./en/us");
+    } else if (location.pathname === "/servicios") {
+      return (location.href = "./en/services");
+    } else if (location.pathname === "/franquicias") {
+      return (location.href = "./en/franchise");
+    } else if (location.pathname === "/franquicias-en-texas") {
+      return (location.href = "./en/franchise-texas");
+    } else if (location.pathname === "/franquicias-en-florida") {
+      return (location.href = "./en/franchise-florida");
+    } else if (location.pathname === "/franquicias-en-california") {
+      return (location.href = "./en/franchise-california");
+    } else if (location.pathname === "/visas-de-inversionistas") {
+      return (location.href = "./en/investor-visa");
+    } else if (location.pathname === "/investor-visa") {
+      return (location.href = "./en/investor-visa");
+    } else if (location.pathname === "/visa-e-2") {
+      return (location.href = "./en/visa-e-2");
+    } else if (location.pathname === "/visa-e-1") {
+      return (location.href = "./en/visa-e-1");
+    } else if (location.pathname === "/visa-eb-5") {
+      return (location.href = "./en/visa-eb-5");
+    } else if (location.pathname === "/visa-l-1") {
+      return (location.href = "./en/visa-l-1");
+    } else if (location.pathname === "/blog") {
+      return (location.href = "./en/blog");
+    } else if (location.pathname === "/faqs") {
+      return (location.href = "./en/faqs");
+    } else if (location.pathname === "/cursos") {
+      return (location.href = "./en/courses");
+    } else if (location.pathname === "/contacto") {
+      return (location.href = "./en/contact");
     } else if (location.pathname === "/") {
-      return (location.href = "./en/index.html");
+      return (location.href = "./en/index");
     }
   } else {
-    if (location.pathname === "/en/us.html") {
+    if (location.pathname === "/en/us") {
       return (location.href = "/nosotros");
-    } else if (location.pathname === "/en/services.html") {
-      return (location.href = "/services.html");
-    } else if (location.pathname === "/en/franchise.html") {
-      return (location.href = "/franchise.html");
-    } else if (location.pathname === "/en/franchise-texas.html") {
-      return (location.href = "/franchise-texas.html");
-    } else if (location.pathname === "/en/franchise-florida.html") {
-      return (location.href = "/franchise-florida.html");
-    } else if (location.pathname === "/en/franchise-california.html") {
-      return (location.href = "/franchise-california.html");
-    } else if (location.pathname === "/en/investor-visa.html") {
-      return (location.href = "/investor-visa.html");
-    } else if (location.pathname === "/en/investor-visa.html") {
-      return (location.href = "/investor-visa.html");
-    } else if (location.pathname === "/en/visa-e-2.html") {
-      return (location.href = "/visa-e-2.html");
-    } else if (location.pathname === "/en/visa-e-1.html") {
-      return (location.href = "/visa-e-1.html");
-    } else if (location.pathname === "/en/visa-eb-5.html") {
-      return (location.href = "/visa-eb-5.html");
-    } else if (location.pathname === "/en/visa-l-1.html") {
-      return (location.href = "/visa-l-1.html");
-    } else if (location.pathname === "/en/blog.html") {
-      return (location.href = "/blog.html");
-    } else if (location.pathname === "/en/faqs.html") {
-      return (location.href = "/faqs.html");
-    } else if (location.pathname === "/en/courses.html") {
-      return (location.href = "/courses.html");
-    } else if (location.pathname === "/en/contact.html") {
-      return (location.href = "/contact.html");
-    } else if (location.pathname === "/en/index.html") {
+    } else if (location.pathname === "/en/services") {
+      return (location.href = "/servicios");
+    } else if (location.pathname === "/en/franchise") {
+      return (location.href = "/franquicias");
+    } else if (location.pathname === "/en/franchise-texas") {
+      return (location.href = "/franquicias-en-texas");
+    } else if (location.pathname === "/en/franchise-florida") {
+      return (location.href = "/franquicias-en-florida");
+    } else if (location.pathname === "/en/franchise-california") {
+      return (location.href = "/franquicias-en-california");
+    } else if (location.pathname === "/en/investor-visa") {
+      return (location.href = "/visas-de-inversionistas");
+    } else if (location.pathname === "/en/visa-e-2") {
+      return (location.href = "/visa-e-2");
+    } else if (location.pathname === "/en/visa-e-1") {
+      return (location.href = "/visa-e-1");
+    } else if (location.pathname === "/en/visa-eb-5") {
+      return (location.href = "/visa-eb-5");
+    } else if (location.pathname === "/en/visa-l-1") {
+      return (location.href = "/visa-l-1");
+    } else if (location.pathname === "/en/blog") {
+      return (location.href = "/blog");
+    } else if (location.pathname === "/en/faqs") {
+      return (location.href = "/faqs");
+    } else if (location.pathname === "/en/courses") {
+      return (location.href = "/cursos");
+    } else if (location.pathname === "/en/contact") {
+      return (location.href = "/contacto");
+    } else if (location.pathname === "/en/index") {
       return (location.href = "/");
     }
   }
